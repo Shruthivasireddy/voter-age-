e1405fdb89b91c38b0dc163b27bb6429f2f6a089
Shruthivasireddy <103738565+Shruthivasireddy@users.noreply.github.com>
2022-04-27 19:25:09 +0530

    created voter age.cpp
    This file prints whether the person is eligible to vote or not

---
+++ voter age.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int age;
+  cout<<"Enter Age of a person:";
+  cin>>age;
+  if(age>=18)
+  {
+cout<<"\nYou are eligible";
+  }
+  else
+  {   
+  cout<<"\n you are not eligible";
+  }  
+  return 0;
+}
